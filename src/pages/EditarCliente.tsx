b87e37bb9d743fe1b31fbf914ea00a74bf9283fa
kelvin21A <antunesbiasibetti@gmail.com>
2025-08-05 16:05:57 -0300

    testes

--- src/pages/EditarCliente.tsx
+++ src/pages/EditarCliente.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import ClientForm from '@/components/forms/ClientForm';
-import { useClient } from '@/hooks/useClient';
+import { useClient } from '@/hooks/useClients';
 import { Loader2 } from 'lucide-react';
 
 const EditarCliente = () => {
