8c577501d2638b58a5f871329f738bc4c7c92cc8
kelvin21A <antunesbiasibetti@gmail.com>
2025-08-05 11:48:59 -0300

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import supabase from '@/lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import { User } from '@supabase/supabase-js';
 import { Tables } from '@/types/database.types';
 
