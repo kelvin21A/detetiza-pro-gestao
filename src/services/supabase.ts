841344d69d563a3af4c49b88aee745cd4c0506bc
kelvin21A <antunesbiasibetti@gmail.com>
2025-08-06 10:08:51 -0300

    Create supabase.ts

---
+++ src/services/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/types/supabase';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('As variáveis VITE_SUPABASE_URL e VITE_SUPABASE_ANON_KEY precisam ser definidas no arquivo .env');
+}
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
